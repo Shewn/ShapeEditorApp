d048738115b2ed22644e1049c14f197c611a2c17
Shewnnn <shewn5041@gmail.com>
2025-04-27 16:20:30 +0800

    chore: Rename import

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,5 +1,5 @@
 import { makeAutoObservable } from "mobx";
-import { Shape, ShapeType } from "../types/shapeType";
+import { Shape, ShapeType } from "../interfaces/shapes";
 
 class ShapeStore {
   shapes: Shape[] = [];
