28a0111fcc261c939550dda91cf472f9616f5e96
Shewnnn <shewn5041@gmail.com>
2025-04-27 16:44:38 +0800

    Shape Drawing App
    Initialise the 'naked' shape drawing apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ShapeControls from "./app/components/shapeControl";
+import ShapeEditorCanvas from "./app/components/shapeCanvas";
+import LayerPanel from "./app/components/shapeLayerPanel";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 20 }}>
+      <h1>Shape & Image Editor</h1>
+      <ShapeControls />
+      <div style={{ display: "flex", alignItems: "flex-start", marginTop: 10 }}>
+        <ShapeEditorCanvas />
+        <LayerPanel />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
