6efbd5fbbc1be6132faaac96545fb4cbc04dd272
Shewnnn <shewn5041@gmail.com>
2025-04-27 16:04:29 +0800

    Initialise Mob Store
    Create a mob store to manage states of shapes

---
+++ src/store/store.ts
@@ -0,0 +1,104 @@
+import { makeAutoObservable } from "mobx";
+
+export type ShapeType = "rect" | "circle" | "image";
+
+export interface RectShape {
+  id: number;
+  type: "rect";
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  color: string;
+}
+
+export interface CircleShape {
+  id: number;
+  type: "circle";
+  x: number;
+  y: number;
+  radius: number;
+  color: string;
+}
+
+export interface ImageShape {
+  id: number;
+  type: "image";
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  image: HTMLImageElement;
+}
+
+export type Shape = RectShape | CircleShape | ImageShape;
+
+class ShapeStore {
+  shapes: Shape[] = [];
+  nextId = 1;
+  selectedShapeType: ShapeType = "rect";
+  selectedColor: string = "#ff0000";
+  uploadedImage: HTMLImageElement | null = null;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  setShapeType(type: ShapeType) {
+    this.selectedShapeType = type;
+  }
+
+  setColor(color: string) {
+    this.selectedColor = color;
+  }
+
+  setUploadedImage(image: HTMLImageElement) {
+    this.uploadedImage = image;
+  }
+
+  addShapeAt(x: number, y: number) {
+    const id = this.nextId++;
+
+    if (this.selectedShapeType === "image" && this.uploadedImage) {
+      this.shapes.push({
+        id,
+        type: "image",
+        x,
+        y,
+        image: this.uploadedImage,
+        width: 100,
+        height: 100,
+      });
+      return;
+    }
+
+    const shape: Shape =
+      this.selectedShapeType === "rect"
+        ? {
+            id,
+            type: "rect",
+            x,
+            y,
+            width: 80,
+            height: 60,
+            color: this.selectedColor,
+          }
+        : { id, type: "circle", x, y, radius: 30, color: this.selectedColor };
+
+    this.shapes.push(shape);
+  }
+
+  removeShapeById(id: number) {
+    this.shapes = this.shapes.filter((shape) => shape.id !== id);
+  }
+
+  undo() {
+    this.shapes.pop();
+  }
+
+  clearShapes() {
+    this.shapes = [];
+  }
+}
+
+export const shapeStore = new ShapeStore();
